f01eb03d3fc2f0f6c110c9ce761544d3c9e3ec80
JustKay1029 <kavyagupta505@gmail.com>
2026-07-23 22:54:11 +0530

    Completed some more steps

--- workshop/salary-tracker.py
+++ workshop/salary-tracker.py
@@ -1,10 +1,16 @@
 class Employee:
+    _base_salaries = {
+        'trainee': 1000,
+        'junior': 2000,
+        'mid-level': 3000,
+        'senior': 4000,
+    }
+
     def __init__(self, name, level):
-        if not isinstance(name,str) or not isinstance(level,str):
-            raise TypeError("'name' and 'level' attribute must be of type 'str'")
-        self._name = name
-        self._level = level
-        
+        self.name = name
+        self.level = level
+        self.salary = Employee._base_salaries[level]
+
     def __str__(self):
         return f'{self.name}: {self.level}'
 
@@ -15,12 +21,48 @@ class Employee:
     def name(self):
         return self._name
 
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError("'name' must be a string.")
+        self._name = new_name
+        print(f"'name' updated to '{self.name}'.")
+
     @property
     def level(self):
         return self._level
 
+    @level.setter
+    def level(self, new_level):
+        if not isinstance(new_level, str):
+            raise TypeError("'level' must be a string.")
+        if new_level not in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{new_level}' for 'level' attribute.")
+        if hasattr(self, '_level') and new_level == self.level:
+            raise ValueError(f"'{self.level}' is already the selected level.")
+        if hasattr(self, '_level') and Employee._base_salaries[new_level] < Employee._base_salaries[self.level]:
+            raise ValueError("Cannot change to lower level.")
+        print(f"'{self.name}hasattr(' promoted to '{new_level}'.")
+        self.salary = Employee._base_salaries[new_level]
+        self._level = new_level
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, new_salary):
+        if not isinstance(new_salary, (int, float)):
+            raise TypeError("'salary' must be a number.")
+        if hasattr(self, '_level'):
+            if new_salary < Employee._base_salaries[self._level]:
+                raise ValueError(f"Salary must be higher than minimum salary ${Employee._base_salaries[self._level]}.")
+        self._salary = new_salary
+        print(f'Salary updated to ${self.salary}.')
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
+print(f'Base salary: ${charlie_brown.salary}')
+charlie_brown.level = 'junior'
 
-
-#this is incomplete, the problem was in the site, everything was done correctly but the site only denied my answer 
+#this is still incomplete the 39th step is showing errors again and again 
