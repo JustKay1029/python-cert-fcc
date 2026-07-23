01c24b5fad9b072b858bcc2ff1a93d6b5087936a
JustKay1029 <kavyagupta505@gmail.com>
2026-07-23 23:00:34 +0530

    This is the final one

--- workshop/salary-tracker.py
+++ workshop/salary-tracker.py
@@ -63,3 +63,5 @@ charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
 print(f'Base salary: ${charlie_brown.salary}')
 charlie_brown.level = 'junior'
+
+#the whole problem was in _level vs level
